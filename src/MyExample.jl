7875652ee6a5a4d8a00ae1d66d589c7e879df6b2
Kartikey <kartikeymullick@gmail.com>
2020-06-20 06:01:37 +0530

    First commit
    Linear expressions as a first commit

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,5 @@
 module MyExample
 
-# Write your package code here.
-
+greet()=print("Hello World")
+include("extra_file.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MyExample
 using Test
 
+my_f(2,1)
+
 @testset "MyExample.jl" begin
-    # Write your tests here.
+    @test my_f(2,1)==5
 end
